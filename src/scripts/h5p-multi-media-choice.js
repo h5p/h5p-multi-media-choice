02411ba5a1185f53a9be9931a01f7cc44be22b2b
Vilde Voss Stabell <Vilde.voss.stabell@joubel.com>
2021-06-23 11:05:35 +0200

    HFP-3301 Add retry button functionality

--- src/scripts/h5p-multi-media-choice.js
+++ src/scripts/h5p-multi-media-choice.js
@@ -34,6 +34,7 @@ export default class MultiMediaChoice extends H5P.Question {
           checkText: 'Check',
           check: 'Check the answers. The responses will be marked as correct, incorrect, or unanswered.',
           showSolution: 'Show the solution. The task will be marked with its correct solution.',
+          retryText: 'Retry',
           retry: 'Retry the task. Reset all responses and start the task over again.',
           result: 'You got @score out of @total points',
         },
@@ -88,6 +89,17 @@ export default class MultiMediaChoice extends H5P.Question {
       { 'aria-label': this.params.l10n.check },
       {}
     );
+
+    this.addButton(
+      'try-again',
+      this.params.l10n.retryText,
+      () => {
+        this.resetTask();
+      },
+      false,
+      { 'aria-label': this.params.l10n.retry },
+      {}
+    );
   }
 
   /**
@@ -100,5 +112,38 @@ export default class MultiMediaChoice extends H5P.Question {
     const textScore = H5P.Question.determineOverallFeedback(this.params.overallFeedback, score / maxScore);
     const ariaMessage = this.params.l10n.result.replace('@score', score).replace('@total', maxScore);
     this.setFeedback(textScore, score, maxScore, ariaMessage);
+
+    if (this.params.behaviour.enableRetry) {
+      this.showButton('try-again');
+    }
+  }
+
+  /**
+   * Resets the options, score and the buttons hidden by showSolutions()
+   *
+   * @see contract at {@link https://h5p.org/documentation/developers/contracts#guides-header-5}
+   */
+  resetTask() {
+    this.content.resetSelections();
+    this.showButton('check-answer');
+    this.hideButton('try-again');
+    this.hideButton('show-solution');
+    this.hideSolutions();
+    this.resetScore();
+    this.removeFeedback();
+  }
+
+  /**
+   * Resets the score and hides the score text
+   */
+  resetScore() {
+    //TODO: Add this when scoring has been implemented
+  }
+
+  /**
+   * Hide the solutions
+   */
+  hideSolutions() {
+    //TODO: Add when solutions has been implemented
   }
 }
